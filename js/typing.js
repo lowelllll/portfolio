b102063671451cb4c4f95b4d97f4c34212d988e3
Lowellll <32219612+lowelllll@users.noreply.github.com>
2018-10-16 00:49:50 +0900

    타이핑 효과 오류 개선과 소개 파트 부분 제작

--- js/typing.js
+++ js/typing.js
@@ -1,44 +1,25 @@
-function changeText(cont1,cont2,speed){
-	var Otext=cont1.text();
-	var Ocontent=Otext.split("");
-	var i=0;
-	function show(){
-		if(i<Ocontent.length)
-		{		
-			cont2.append(Ocontent[i]);
-			i=i+1;
-		};
-	};
-		var Otimer=setInterval(show,speed);	
-};
-$(document).ready(function(){
-  changeText($(".origin-text"),$("#text"),150);
-  changeText($(".origin-text2"),$("#text2"),150);  
-});
+var text = "안녕하세요. <br> 항상 성장하고 싶은 이예진입니다.";
 
+var cnt = 0;
+var speed = 100; //글자가 찍히는 속도
+var timer1 = null;
+var word = "";
 
-// var text = "안녕하세요.<br> 성장하는 것을 좋아하는 이예진입니다.";
+function typing(){
+    word = text.substring(0, cnt);
+    if(cnt < text.length) {
+      word += "|";
+    }
+    if(text.substring(cnt-1,cnt)!='<'){
+      document.getElementById('text').innerHTML =  word;
+    }
+    cnt++;
+    timer1 = setTimeout('typing()', speed);
+    speed = 100;
+    if(text.length < cnt){
+        clearTimeout(timer1);
+        cnt = 0;
+    }
+  }
 
-// var cnt = 0;
-// var speed = 100; //글자가 찍히는 속도
-// var timer1 = null;
-// var word = "";
-
-// function gogogo(){
-//     word = text.substring(0, cnt);
-//     document.getElementById('text').innerHTML =  word;
-//     console.log(text.substring(cnt-1,cnt));
-//     cnt++;
-//     timer1 = setTimeout('gogogo()', speed);
-//     speed = 100;
-//     if(text.length < cnt){
-//         // 아래 주석으로 처리된 부분을 지우면 한번만 실행됩니다.
-//         clearTimeout(timer1)
-//         cnt = 0;
-//     }
-//   }
-
-//   gogogo();
-// // $(document).ready(function(){
-// //   gogogo();
-// // });
+  typing();
